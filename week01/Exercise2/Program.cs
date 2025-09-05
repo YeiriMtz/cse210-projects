68537a1565d76b552560f5b42c24fa2963b3cfe3
YeiriMtz <yeirimartinez80@gmail.com>
2025-09-05 00:38:50 -0600

    excercise 2 from week 1, DONE

--- week01/Exercise2/Program.cs
+++ week01/Exercise2/Program.cs
@@ -4,6 +4,59 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise2 Project.");
+        Console.WriteLine("");
+
+        // USER INPUT
+        Console.Write("What was your grade percentage? ");
+        string user_grade = Console.ReadLine();
+
+        // CONVERTING INPUT INTO INTEGER
+        int grade = int.Parse(user_grade);
+
+        // STEP #3, Changing code
+        string letter;
+
+        // A
+        if (grade >= 90)
+        {
+            letter = "A";
+        }
+        // B
+        else if (grade >= 80)
+        {
+            letter = "B";
+        }
+        // C
+        else if (grade >= 70)
+        {
+            letter = "C";
+        }
+        // D
+        else if (grade >= 60)
+        {
+            letter = "D";
+        }
+        // F
+        else
+        {
+            letter = "F";
+        }
+
+        // PRINTING GRADE
+        Console.WriteLine($"Your grade is {letter}");
+
+        // PASSED OR NOT MESSAGE
+
+        if (grade >= 70)
+        {
+            Console.WriteLine("Congratulations! YOU PASSED!");
+        }
+
+        else
+        {
+            Console.WriteLine("YOU FAILED, keep trying");
+        }
+        
+        Console.WriteLine("");
     }
 }

bb576d5713f4194f869982126f817a8db420d5c8
YeiriMtz <yeirimartinez80@gmail.com>
2025-09-15 00:00:28 -0600

    dhdhd

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -24,4 +24,46 @@ class Journal
     {
         _entries.Add(entry);
     }
+
+    public void DisplayAll()
+    {
+        foreach (Entry e in _entries)
+        {
+            e.Display();
+        }
+    }
+
+    public void SaveToFile(string filename)
+    {
+        using (StreamWriter outputFile = new StreamWriter(filename))
+        {
+            foreach (Entry e in _entries)
+            {
+                outputFile.WriteLine($"{e.Date} | {e.Response}");
+            }
+        }
+    }
+
+    public void LoadFromFile(string filename)
+    {
+        _entries.Clear();
+        string[] lines = File.ReadAllLines(filename);
+
+        foreach (string line in lines)
+        {
+            string[] parts = line.Split(' | ');
+            if (parts.Length == 3)
+            {
+                Entry newEntry = new Entry
+                {
+                    Date = parts[0],
+                    Prompt = parts[1],
+                    Response = parts[2]
+                };
+                _entries.Add(newEntry);
+            }
+        }
+    }
+
+    class Program
 }

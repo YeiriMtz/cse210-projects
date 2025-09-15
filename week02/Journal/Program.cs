e6898f52e32154e4f52e0496a50757f6aec5eda4
YeiriMtz <yeirimartinez80@gmail.com>
2025-09-14 23:44:50 -0600

    starting journal program

--- week02/Journal/Program.cs
+++ week02/Journal/Program.cs
@@ -1,9 +1,27 @@
 using System;
+using System.Collections.Generic;
+using System.IO;
 
-class Program
+class Entry
 {
-    static void Main(string[] args)
+    public string Date { get; set; }
+    public string Prompt { get; set; }
+    public string Response { get; set; }
+
+    public void Display()
     {
-        Console.WriteLine("Hello World! This is the Journal Project.");
+        Console.WriteLine($"{Date} - {Prompt}");
+        Console.WriteLine(Response);
+        Console.WriteLine();
+    }
+}
+
+class Journal
+{
+    private List<Entry> _entries = new List<Entry>();
+
+    public void AddEntry(Entry entry)
+    {
+        _entries.Add(entry);
     }
 }

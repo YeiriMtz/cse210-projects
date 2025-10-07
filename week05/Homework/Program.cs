5fa639f73be9fe2acda3de20537a5afb5c0d71f9
YeiriMtz <yeirimartinez80@gmail.com>
2025-10-07 15:53:57 -0600

    dddd

--- week05/Homework/Program.cs
+++ week05/Homework/Program.cs
@@ -7,20 +7,23 @@ namespace Homework.week05
         static void Main(string[] args)
         {
             // Test base class
-            Assignment assignment = new Assignment("Samuel Bennett", "Multiplication");
+            Console.WriteLine("---------------------------------------");
+            Assignment assignment = new Assignment("\nYeiri Martinez", "Multiplication");
             Console.WriteLine(assignment.GetSummary());
             Console.WriteLine();
 
             // Test MathAssignment
-            MathAssignment math = new MathAssignment("Roberto Rodriguez", "Fractions", "7.3", "8-19");
+            MathAssignment math = new MathAssignment("Lara Da Silva Soares", "Fractions", "7.3", "8-19");
             Console.WriteLine(math.GetSummary());
             Console.WriteLine(math.GetHomeworkList());
             Console.WriteLine();
 
             // Test WritingAssignment
-            WritingAssignment writing = new WritingAssignment("Mary Waters", "European History", "The Causes of World War II");
+            WritingAssignment writing = new WritingAssignment("Oliver Martinez", "European History", "The Causes of World War II");
             Console.WriteLine(writing.GetSummary());
             Console.WriteLine(writing.GetWritingInformation());
+            Console.WriteLine("");
+            Console.WriteLine("---------------------------------------");
         }
     }
 }
